fbdbea78381b64361c55ac413c8ca5143a6d0a79
DylanRothbauer <djrothbauer@gmail.com>
2024-09-13 12:05:54 -0500

    Got human to play BLOOD and die

--- Scripts/human.gd
+++ Scripts/human.gd
@@ -1,8 +1,16 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var animation = $AnimatedSprite2D2
+
 
 func _on_body_entered(body: Node2D) -> void:
-	# Add time for eating human
+	# Play animation
+	animation.play("BLOOD")
 	
+	# Add time for eating human
+
+
+func _on_animated_sprite_2d_2_animation_finished() -> void:
+	# Kill player
 	queue_free()
