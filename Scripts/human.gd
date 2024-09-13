d57eb83de55d462707e4024e3ca62adcfa167b3d
DylanRothbauer <djrothbauer@gmail.com>
2024-09-13 13:27:53 -0500

    Shark gets bigger when eats human

--- Scripts/human.gd
+++ Scripts/human.gd
@@ -1,18 +1,23 @@
 extends Area2D
 
 @onready var timer = get_node("/root/Game/Timer")
-
-@onready var animation = $AnimatedSprite2D2
-
+@onready var animationHuman = $AnimatedSprite2D2
+@onready var animationShark = get_node("/root/Game/Shark/AnimatedSprite2D")
+var xScale = 1
+var yScale = 1
+var scalar = .5 # Can adjust this when we refine/polish
 
 func _on_body_entered(body: Node2D) -> void:
 	# Play animation
-	animation.play("BLOOD")
+	animationHuman.play("BLOOD")
 	
 	# Add time for eating human
 	var timeLeft = timer.get_time_left()
 	# timer.set_wait_time(timeLeft + 3.00)
 	timer.start(timeLeft + 3.00)
+	
+	# Increase size of shark
+	animationShark.set_scale(Vector2(xScale + scalar,yScale + scalar))
 
 func _on_animated_sprite_2d_2_animation_finished() -> void:
 	# Kill player
